5675d8036813f8a0b1c087714e8bf8ddb2c0621f
baunilha <brucalheiros@gmail.com>
2012-11-28 13:47:05 -0500

    twilio activated

--- app.py
+++ app.py
@@ -17,7 +17,18 @@ app = Flask(__name__)   # create our flask app
 # --------- Routes ----------
 @app.route('/')
 def ghost_demo():
-	return render_template('index.html')
+	account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+    auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+
+    # This is a special Quickstart application sid - or configure your own
+    # at twilio.com/user/account/apps
+    application_sid = os.environ.get('TWILIO_APP_SID')
+
+    capability = TwilioCapability(account_sid, auth_token)
+    capability.allow_client_outgoing(application_sid)
+    token = capability.generate()
+
+	return render_template('index.html', token=token)
 
 
 @app.route("/demo", methods=['GET', 'POST'])
@@ -27,9 +38,6 @@ def hello_monkey():
     resp.say("Hello Monkey")
  
     return str(resp)
- 
-if __name__ == "__main__":
-    app.run(debug=True)
 
 
 @app.route('/twilio', methods=['GET','POST'])
@@ -70,7 +78,6 @@ def twilio():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
-
 
 
 # --------- Server On ----------
